6cb4b62be04d29242eb958b7185a0d91e30f92cb
saraiesco <saraiescobar@google.com>
2023-10-10 13:05:30 -0500

    Musician model

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,15 @@
-const {Sequelize, sequelize} = require('../db');
+const {Sequelize, sequelize, DataTypes, Model} = require('../db');
 
 // TODO - define the Musician model
-let Musician;
+class Musician extends Model{}
+
+Band.init({
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING,
+},{
+    sequelize: sequelize,
+    modelName: "Musician"
+})
 
 module.exports = {
     Musician
