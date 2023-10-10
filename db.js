f37303287d9d6b25c10003069d1babb5e1847d70
saraiesco <saraiescobar@google.com>
2023-10-10 11:49:06 -0500

    Band model

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
 const path = require('path');
-const { Sequelize, Model } = require('sequelize');
+const { Sequelize, Model,DataTypes } = require('sequelize');
 
 // TODO - create the new sequelize connection
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.join(__dirname, 'sequelize.sqlite'),
+})
 
 module.exports = {
     sequelize,
-    Sequelize
+    Sequelize,
+    DataTypes,
+    Model,
 };
