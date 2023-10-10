5e4f80ed8ec78c1b739f884244a2eba6b1f0f7b4
saraiesco <saraiescobar@google.com>
2023-10-10 13:18:20 -0500

    Song model

--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,16 @@
-const {Sequelize, sequelize} = require('../db');
+const {Sequelize, sequelize, DataTypes, Model} = require('../db');
 
 // TODO - define the Song model
-let Song;
+class Song extends Model{}
+
+Song.init({
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    length: DataTypes.INTEGER,
+},{
+    sequelize: sequelize,
+    modelName: "Song"
+})
 
 module.exports = {
     Song
