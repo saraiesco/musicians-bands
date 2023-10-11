d013ce4d965e392f8f811692d395cc9ccf68a4b7
saraiesco <saraiescobar@google.com>
2023-10-11 15:33:42 -0500

    test 1

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
 
+//one to many
+Band.hasMany(Musician);
+Musician.belongsTo(Band);
+
+//many to many
+Song.belongsToMany(Band, {through: "bandsong"});
+Band.belongsToMany(Song, {through: "bandsong"});
 
 module.exports = {
     Band,
--- index.test.js
+++ index.test.js
@@ -51,3 +51,51 @@ describe('Band, Musician, and Song Models', () => {
         expect(deletedMusician).toBe(foundMusician);
     })
 })
+
+
+
+describe('test associations', () => {
+
+
+    beforeAll(async () => {
+    await db.sync({ force: true });
+        // const Queen = await Band.create({name: "Queen", genre: "rock"})
+        // const band = await Band.create({name: "Foo Fighters", genre: "Rock"})
+        // const band1 = await Band.create({ name: "Vampire Weekend",genre: "Indie"})
+        // const beet = await Musician.create({ name: "Beethovan", instrument: "Piano"})
+        // const musician1 = await Musician.create({name: "Lindsey Sterling", instrument: "Violin"})
+        // const song = await Song.create({title: "26",year: 2018, length: 4})
+        // const song1 = await Song.create({title: "Spice Girl",year: 2018,length: 4})
+    });
+    
+    
+    //one to many
+    test('band has many musicians', async () => {
+        const Queen = await Band.create({name: "Queen", genre: "rock"})
+        const band1 = await Band.create({ name: "Vampire Weekend",genre: "Indie"})
+        const beet = await Musician.create({ name: "Beethovan", instrument: "Piano"})
+        const musician1 = await Musician.create({name: "Lindsey Sterling", instrument: "Violin"})
+        const bands = await Band.findAll();
+        const foundBand = await bands[0]
+        const musicians = await Musician.findAll();
+        await foundBand.setMusicians(musicians[0],musicians[1])
+        let bandsMusicians = await foundBand.getMusicians();
+        console.log(JSON.stringify(bandsMusicians,null,2));
+        // expect().toBe();
+    })
+
+    //many to many
+    test('songs has many bands and bands have many song', async () => {
+    // Band.create() to make some bands. Use the data you’ve added in previous tests you’ve created!
+    // Create at least 2 songs
+    // For one band, add both songs
+    // For each of the songs, use something like foundBand.getSongs() to check that they have been added correctly!
+
+    // Song.create() to make some songs. Use the data you’ve added in previous tests you’ve created!
+    // Create at least 2 bands
+    // For one song, add both bands
+    // For each of the bands, use something like foundSong.getBands() to check that they have been added correctly!
+        expect().toBe();
+    })
+})
+    
